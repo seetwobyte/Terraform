5b92fb597497ca740ec76d61bad32ef63c484a2c
seetwobyte <successscotty@gmail.com>
2018-02-18 14:16:39 -0500

    fixed Securitygroups

--- demo-8/elb.tf
+++ demo-8/elb.tf
@@ -4,7 +4,7 @@
 resource "aws_elb" "web-elb" {
   name = "web-elb"
   subnets = ["${aws_subnet.web-a.id}", "${aws_subnet.web-b.id}"]
-  security_groups = ["${aws_security_group.webServer-SG.id}"]
+  security_groups = ["${aws_security_group.allow-ssh.id}"]
   internal = true
 
  listener {
@@ -29,53 +29,27 @@ resource "aws_elb" "web-elb" {
   }
 }
 
-resource "aws_elb" "public-elb" {
-  name = "public-elb"
-  security_groups = ["${aws_security_group.public-SG.id}"]
-  subnets = ["${aws_subnet.public-a.id}","${aws_subnet.public-b.id}"]
-  internal = false
-
-  "listener" {
-    instance_port = 80
-    instance_protocol = "tcp"
-    lb_port = 80
-    lb_protocol = "tcp"
-  }
-  health_check {
-    healthy_threshold = 2
-    interval = 30
-    target = "HTTP:80/"
-    timeout = 30
-    unhealthy_threshold = 2
-  }
-  cross_zone_load_balancing = true
-  connection_draining = true
-  connection_draining_timeout = 400
-  tags {
-    Name = "public-elb"
-  }
-}
 
 
 resource "aws_elb" "private-elb" {
   name = "public-elb"
-  security_groups = ["${aws_security_group.privateSG.id}"]
+  security_groups = ["${aws_security_group.allow-ssh.id}"]
   subnets = ["${aws_subnet.private-a.id}","${aws_subnet.private-a.id}"]
   internal = true
 
 
-  "listener" {
+listener {
     instance_port = 80
-    instance_protocol = "tcp"
+    instance_protocol = "http"
     lb_port = 80
-    lb_protocol = "tcp"
+    lb_protocol = "http"
   }
   health_check {
     healthy_threshold = 2
-    interval = 30
-    target = "HTTP:80/"
-    timeout = 30
     unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/"
+    interval = 30
   }
   cross_zone_load_balancing = true
   connection_draining = true
@@ -92,20 +66,18 @@ resource "aws_elb" "data-elb" {
   subnets = ["${aws_subnet.data-a.id}", "${aws_subnet.data-b.id}"]
   internal = true
 
-  "listener" {
+listener {
     instance_port = 80
-    instance_protocol = "tcp"
+    instance_protocol = "http"
     lb_port = 80
-    lb_protocol = "tcp"
+    lb_protocol = "http"
   }
-
-
   health_check {
     healthy_threshold = 2
-    interval = 30
-    target = "http:80/"
-    timeout = 30
     unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/"
+    interval = 30
   }
   cross_zone_load_balancing = true
   connection_draining = true
