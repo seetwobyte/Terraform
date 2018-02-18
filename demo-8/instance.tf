f53f1c778ca27bbb4faa841a9afecc308e526da9
seetwobyte <successscotty@gmail.com>
2018-02-18 13:57:43 -0500

    fixed output

--- demo-8/instance.tf
+++ demo-8/instance.tf
@@ -16,16 +16,21 @@ resource "aws_instance" "example" {
 }
 
 resource "aws_instance" "remote_access" {
-  ami = "${lookup(lookup(var.AMIS, var.AWS_REGION ))}"
+  ami = "${lookup(var.AMIS, var.AWS_REGION )}"
   instance_type = "t2.micro"
   monitoring = false
   count = 1
   subnet_id = "${aws_subnet.public-a.id}"
-vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]}
-key_name =  "${aws_key_pair.mykeypair.key_name}"
+  vpc_security_group_ids = [
+    "${aws_security_group.allow-ssh.id}"]
+  key_name = "${aws_key_pair.mykeypair.key_name}"
 
   tags {
     Name = "remote"
   }
 }
+
+
+
+
 
