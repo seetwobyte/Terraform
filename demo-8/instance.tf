e30256410ca1fd52194d024fc62233a447e872dc
seetwobyte <successscotty@gmail.com>
2018-03-02 18:50:45 -0500

    # separate the Instances

--- demo-8/instance.tf
+++ demo-8/instance.tf
@@ -10,10 +10,47 @@ resource "aws_instance" "example" {
   # the public SSH key
   key_name = "${aws_key_pair.mykeypair.key_name}"
   tags {
-    Name = "example1A"
+    Name = "Linux1"
   }
 
 }
+
+resource "aws_instance" "example" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  monitoring = true
+  count = 1
+  # the VPC subnet
+  subnet_id = "${aws_subnet.web-a.id}"
+  # the security group
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+  # the public SSH key
+  key_name = "${aws_key_pair.mykeypair.key_name}"
+  tags {
+    Name = "Linux2"
+  }
+
+}
+
+resource "aws_instance" "example" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  monitoring = true
+  count = 1
+  # the VPC subnet
+  subnet_id = "${aws_subnet.web-a.id}"
+  # the security group
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+  # the public SSH key
+  key_name = "${aws_key_pair.mykeypair.key_name}"
+  tags {
+    Name = "Linux3"
+  }
+
+}
+
+
+
 
 resource "aws_instance" "remote_access" {
   ami = "${lookup(var.AMIS, var.AWS_REGION )}"
