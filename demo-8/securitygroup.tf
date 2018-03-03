b3862c8faa489d101d40020397c74b4d689145ae
seetwobyte <successscotty@gmail.com>
2018-03-02 19:24:44 -0500

    Create ELB-SG

--- demo-8/securitygroup.tf
+++ demo-8/securitygroup.tf
@@ -20,3 +20,32 @@ tags {
   }
 }
 
+resource "aws_security_group" "ELB-SG" {
+  vpc_id = "${aws_vpc.bancroft.id}"
+  name = "allow-ssh"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["196.52.2.15/32"]
+  }
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["196.52.2.15/32"]
+  }
+tags {
+    Name = "allow-ssh"
+  }
+}
+
