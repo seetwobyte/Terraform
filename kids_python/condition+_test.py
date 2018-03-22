b7716a9f74221e62c4d19fed9bde0e12a5eb335c
seetwobyte <successscotty@gmail.com>
2018-03-21 20:28:12 -0400

    # the test 1

---
+++ kids_python/condition+_test.py
@@ -0,0 +1,19 @@
+# letter test
+
+pointspossible = 100
+
+score = 75
+
+percent = score / pointspossible
+
+
+print("Percentage is {}".format(percent))
+
+student = "Scott"
+
+
+if 1 >= percent >= 0.9:
+    grade = "A"
+elif 0.9 >= percent >= 0.8:
+    graade = "B"
+elif 0.8 >= percent >= 0.7:
