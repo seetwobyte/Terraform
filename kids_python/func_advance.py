c7959948eb2a695628408ca43c0a38eaefbad2cc
seetwobyte <successscotty@gmail.com>
2018-03-24 13:04:59 -0400

    # parameters

---
+++ kids_python/func_advance.py
@@ -0,0 +1,6 @@
+# advance func
+
+def madlib(name, verb, noun):
+    print(name + " was " + verb + " with a " + noun)
+
+madlib("Scott", "Running", "Cheeze")
---
+++ kids_python/func_advnace1.py
@@ -0,0 +1,6 @@
+# advance func with advance parameters
+
+def madlib(name="sean", verb="running", noun="cheeze"):
+    print(name + " was " + verb + " with a " + noun)
+
+madlib(verb="run")
