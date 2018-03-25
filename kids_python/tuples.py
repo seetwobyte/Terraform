12647b5ba12fff1035dde99e232a0c1c72d8981b
seetwobyte <successscotty@gmail.com>
2018-03-24 22:22:08 -0400

    # kids python

---
+++ kids_python/tuples.py
@@ -0,0 +1,10 @@
+# tuples is a list that cannot be change.
+# square brackets parentesis
+days = ("Monday", "Tuesday")
+friends = ["Natalie", "Mike", "Scott"]
+
+print(type(days))
+
+print(type(friends))
+
+
